7935981888c133bf75f9c81c1481be4391c97def
psd90 <pdhillon.dev@gmail.com>
2021-09-14 16:12:31 +0100

    delete a movie with a button rendered on each movieListItem

--- app/components/movie-list/movie-list-item/component.js
+++ app/components/movie-list/movie-list-item/component.js
@@ -1,9 +1,27 @@
+import { action } from '@ember/object';
+import { deleteDoc } from 'firebase/firestore';
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import podNames from 'ember-component-css/pod-names';
+
+
 
 export default class MovieListItem extends Component {
   styleNamespace = podNames['movie-list/movie-list-item'];
 
+  @tracked movies;
+
+  @action async delete() {
+    try {
+          await deleteDoc(this.args.movie.ref);
+          window.location.reload(true);
+        }
+     catch (error) {
+      this.errorMessage = error.message;
+    }
+  }
+
+  
   get movie() {
     return this.args.movie.data();
   }
