50a6f3184f3109ebcc8177195def447c05801ea5
psd90 <pdhillon.dev@gmail.com>
2021-09-14 19:56:35 +0100

    edit movie description, rating and title with buttons rendered next to each respective element

--- app/components/movie-list/movie-list-item/component.js
+++ app/components/movie-list/movie-list-item/component.js
@@ -1,5 +1,5 @@
 import { action } from '@ember/object';
-import { deleteDoc } from 'firebase/firestore';
+import { deleteDoc, updateDoc } from 'firebase/firestore';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import podNames from 'ember-component-css/pod-names';
@@ -11,16 +11,39 @@ export default class MovieListItem extends Component {
 
   @tracked movies;
 
-  @action async delete() {
+  @action async edit(event) {
     try {
-          await deleteDoc(this.args.movie.ref);
-          window.location.reload(true);
-        }
-     catch (error) {
+
+      const target = event.target.id;
+      const result = window.prompt(`New ${target}`);
+      const edit = { [`${target}`]: result };
+
+
+      if (target === 'rating' && result >= 0 && result <= 5) {
+        await updateDoc(this.args.movie.ref, edit);
+        window.location.reload(true);
+      } else if (
+        (target === 'desc' || target === 'title') &&
+        result.length > 0
+      ) {
+        await updateDoc(this.args.movie.ref, edit);
+        window.location.reload(true);
+      }
+    } catch (error) {
       this.errorMessage = error.message;
     }
   }
-
+  
+  
+  @action async delete() {
+    try {
+      await deleteDoc(this.args.movie.ref);
+      window.location.reload(true);
+    }
+    catch (error) {
+      this.errorMessage = error.message;
+    }
+  }
   
   get movie() {
     return this.args.movie.data();
